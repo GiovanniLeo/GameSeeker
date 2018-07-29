b89453b29034693ae416559ba033b3d2300f1ca2
Giovanni Leo <giovannileo595@gmail.com>
2018-07-29 18:10:54 +0200

    Aggiunto il codice per ottenere il titolo e la piattaforma da Amazon

--- src/AmazonScraping.java
+++ src/AmazonScraping.java
@@ -19,7 +19,7 @@ public class AmazonScraping {
 	public String onlyGameUrl(String searchQuery)
 	{
 		String gameUrl = "https://www.amazon.it/s/ref=sr_nr_n_3?fst=as%3Aoff&rh=n%3A412603031%2Cn%3A13900044031%2Ck%3A"
-						+searchQuery+"&keywords="+searchQuery+"&ie";
+						+searchQuery+"&page=1&keywords="+searchQuery+"&ie";
 		System.out.println(gameUrl);
 		return gameUrl;
 	}
@@ -34,8 +34,28 @@ public class AmazonScraping {
 			{
 				Element imgEl = listOfResult.select("#result_"+i+"> div > div > div >"
 								+ "div.a-fixed-left-grid-col.a-col-left > div > div > a > img").first();
+				Element titleEl = listOfResult.select("#result_"+i+"> div > div > div >"
+									+ " div.a-fixed-left-grid-col.a-col-right > div.a-row.a-spacing-small > "
+									+ "div:nth-child(1) > a > h2").first();
+				Element plattform = listOfResult.select("#result_"+i+" > div > div > div > div.a-fixed-left-grid-col.a-col-right"
+									+ " > div:nth-child(2) > div.a-column.a-span7 > table > tbody >"
+									+ " tr.a-spacing-none.s-table-twister-row-no-border.s-table-twister-row > "
+									+ "td:nth-child(1) > div:nth-child(1) > a").first();
+				if(plattform == null)
+				{
+					plattform = listOfResult.select("#result_"+i+" > div > div > div >"
+							+ " div.a-fixed-left-grid-col.a-col-right > div:nth-child(2) >"
+							+ " div.a-column.a-span7 > div:nth-child(1) > a").first();
+					if(plattform == null) continue;
+				}
+				String title = titleEl.text();
 				String imgUrl = imgEl.attr("src");
-				System.out.println(imgUrl);
+				String linkRef = plattform.attr("href");
+				String plattformTitle = plattform.attr("title");
+				if(plattformTitle.equalsIgnoreCase("pc")) {
+				System.out.println("result_"+i+"{\n"+imgUrl+"\n"+title+"\n"
+						+linkRef+"\n"+plattformTitle+"\n"+"\n}\n");
+				}
 				
 			}
 		

edb4e1a3b81f7a13986cb3e85c0b01fbb678a4d3
Giovanni Leo <giovannileo595@gmail.com>
2018-08-10 16:29:26 +0200

    Wrapper youtube finito

--- src/wrappers/Test.java
+++ src/wrappers/Test.java
@@ -1,12 +1,18 @@
 package wrappers;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import utility.Result;
 
 public class Test {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 //		Amazon amz = new Amazon();
 //		Result minResult = amz.searchResult("dark souls");
 //		if(minResult != null)
@@ -29,14 +35,20 @@ public class Test {
 //		}
 		
 		
-		InstantGaming ig = new InstantGaming();
-		 ArrayList<Result> results = ig.searchResults("Airport Simulator");
-		 
-		 for (int i = 0; i < results.size(); i++) {
-			
-			System.out.println(results.get(i).instantGamingToString());
-		}
+//		InstantGaming ig = new InstantGaming();
+//		 ArrayList<Result> results = ig.searchResults("Airport Simulator");
+//		 
+//		 for (int i = 0; i < results.size(); i++) {
+//			
+//			System.out.println(results.get(i).instantGamingToString());
+//		
 		
+		YouTube yt = new YouTube();
+		HashMap<String, String> map = yt.searchResults("Dark Souls");
+		System.out.println(map.get(YouTube.GP));
+		System.out.println(map.get(YouTube.VD));
+		
+
 	}
 
 }

5dcb924e96989a960acf93eae0998ad395f657fa
Giovanni Leo <giovannileo595@gmail.com>
2018-08-12 16:25:28 +0200

    Aggiunta computazione delle distanza e fatta una piccola prova con i thread

---
+++ src/task/SteamTask.java
@@ -0,0 +1,24 @@
+package task;
+
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+
+import utility.Result;
+import wrappers.Steam;
+
+public class SteamTask implements Callable<ArrayList<Result>> {
+
+	private String searchString;
+	private Steam st;
+	
+	public  SteamTask(String searchString) {
+		this.searchString = searchString;
+		st = new Steam();
+	}
+	@Override
+	public ArrayList<Result> call() throws Exception {
+		
+		return st.searchResul(searchString);
+	}
+
+}

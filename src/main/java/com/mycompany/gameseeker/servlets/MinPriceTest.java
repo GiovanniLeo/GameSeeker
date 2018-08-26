922454f53d15ac2c6a904229fc4a75b661ae4e20
Raffaele Ceruso <raffaeleceruso@gmail.com>
2018-08-26 12:12:09 +0200

    MinPriceTest bozza

--- src/main/java/com/mycompany/gameseeker/servlets/MinPriceTest.java
+++ src/main/java/com/mycompany/gameseeker/servlets/MinPriceTest.java
@@ -12,7 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import com.mycompany.gameseeker.mediator.Mediator;
 /**
  *
  * @author raffa
@@ -32,17 +32,12 @@ public class MinPriceTest extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        String title = request.getParameter("titolo");
+        Mediator md = new Mediator();
+        md.selectElements(title);
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet MinPriceTest</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet MinPriceTest at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+
         }
     }
 
